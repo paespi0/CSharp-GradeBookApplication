99eaefc221abd6da93547bc1da1dd4df4f7d41c5
Pedro Espinoza <paespi0@msn.com>
2019-04-10 16:32:56 -0500

    "Completed the module"

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -39,10 +39,11 @@ namespace GradeBook.UserInterfaces
                 Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
             }
-            var name = parts[2];
+            var name = parts[1];
+            var type = parts[2].ToLower();
             BaseGradeBook gradeBook;
 
-            switch (name)
+            switch (type)
             {
                 case "standard":
                     gradeBook = new StandardGradeBook(name);
@@ -51,9 +52,8 @@ namespace GradeBook.UserInterfaces
                     gradeBook = new RankedGradeBook(name);
                     break;
                 default:
-                    Console.WriteLine("{0} is not a supported type of gradebook, please try again", name);
+                    Console.WriteLine("{0} is not a supported type of gradebook, please try again", type);
                     return;
-
             }
 
             Console.WriteLine("Created gradebook {0}.", name);
